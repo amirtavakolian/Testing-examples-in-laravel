c82d8ce165a81513500bda158986f992778f85d7
Amir Tavakolian <rezatva@gmail.com>
2024-03-04 16:21:12 +0330

    add new column to user migration and all test failed - no default value for type column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->now();
             $table->string('password');
+            $table->string('type');
             $table->rememberToken();
             $table->timestamps();
         });
